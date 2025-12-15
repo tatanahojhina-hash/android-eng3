0aff6681eb3e016910ab425368474ac381cac4e1
Tatiana khoykhina <tatanahojhina@gmail.com>
2025-12-16 01:03:41 +0300

    removal

--- main.java
+++ main.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+        
+
+        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in); 
  
         System.out.print("Введите цену товара (в руб.):"); 
