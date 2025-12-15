95266ee66f1868ba1f9db03824cfd2b3f740d9f9
Tatiana khoykhina <tatanahojhina@gmail.com>
2025-12-15 15:23:34 +0300

    input price

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+    Public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+ 
+        System.out.print("Введите цену товара (в руб.):"); 
+        Int price = scanner.nextInt();
+  
